9b34902a90e2f349a7c4772e09b8f82b40af6527
Babak Amin Azad <babak.silverfox@gmail.com>
2016-10-10 13:45:08 +0330

    Create CVEDetails_Merge_TSVs.user.js
    Using this script, you can download all pages of vulnerabilities under http://www.cvedetails.com/vulnerability-list.php page in a single tsv file.

---
+++ CVEDetails_Merge_TSVs.user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name        CVEDetails Merge TSVs
+// @namespace   http://www.cvedetails.com/
+// @description Merges all TSVs and downloads them as a single file (Using Download Results)
+// @include     http://www.cvedetails.com/vulnerability-list.php*
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// ==/UserScript==
+
+function MergePages(element) {
+	var links = FindPageLinks(element);
+	console.log('[+] Links Fetched');
+	console.log(links);
+
+	for (var i = 1; i <= links.length - 1; i++) {
+		console.log('[+] Sending XHR for page ' + (i + 1));
+		SendXHR(links[i].href);
+	}
+}
+
+function FindPageLinks(element) {
+	console.log('[+] Method Call : FindPageLinks');
+	console.log('[+] Variable Passed :');
+	console.log(element);
+	var links = $(element).find('a[title*=\'Go to page\']');
+	console.log('[+] Page Links Derived');
+	return links;
+}
+
+function GetTable(element) {
+	table = $(element).find('#vulnslisttable > tbody > tr').not(':first');
+	console.log('[+] Retrieved Table');
+	return table;
+}
+
+function UpdatePage(table_rows) {
+	console.log('[+] Adding rows to current page');
+	$('#vulnslisttable tbody').append(table_rows);
+}
+
+function SendXHR(url) {
+	$.get(url, function(data) {
+  		var oPage = data,   // page contents
+        table_rows = GetTable(oPage); // analyse the contents
+        UpdatePage(table_rows); // do current page modifications
+	});
+}
+
+MergePages($('#pagingt'));
